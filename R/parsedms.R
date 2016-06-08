0ce600e8f58cf50dfec5328157259a9256bbf7ae
Pieter Provoost <pieterprovoost@gmail.com>
2016-06-08 15:50:34 +0200

    fixed parsing of decimal coordinate

--- R/parsedms.R
+++ R/parsedms.R
@@ -10,42 +10,45 @@ parsedms <- function(input) {
   pattern <- "(-?\\d+(?:\\.\\d+)?)\\s*[°o]\\s*(?:(\\d+(?:\\.\\d+)?)\\s*[´`'ʹʻʼ′’]\\s*(?:(\\d+(?:\\.\\d+)?)\\s*(?:″|ʺ|\"|”|´´|``|''|ʹʹ|ʻʻ|ʼʼ|′′|’’)?)?)?\\s*([NSEW])?"
   matches <- str_match_all(input, regex(pattern, ignore_case = TRUE))
 
-  for (m in 1:length(matches)) {
+  if (length(matches) > 0) {
+    for (m in 1:length(matches)) {
 
-    coord <- list(lon = NA, lat = NA)
-    match <- matches[[m]]
+      coord <- list(lon = NA, lat = NA)
+      match <- matches[[m]]
 
-    for (n in 1:nrow(match)) {
+      if (nrow(match) > 0) {
+        for (n in 1:nrow(match)) {
 
-      d <- as.numeric(match[n, 2])
-      m <- as.numeric(match[n, 3])
-      s <- as.numeric(match[n, 4])
-      h <- match[n, 5]
+          d <- as.numeric(match[n, 2])
+          m <- as.numeric(match[n, 3])
+          s <- as.numeric(match[n, 4])
+          h <- match[n, 5]
 
-      if (!is.na(d)) {
-        dd <- abs(d)
-        if (!is.na(m)) {
-          dd <- dd + m / 60
-          if (!is.na(s)) {
-            dd <- dd + s / 3600
+          if (!is.na(d)) {
+            dd <- abs(d)
+            if (!is.na(m)) {
+              dd <- dd + m / 60
+              if (!is.na(s)) {
+                dd <- dd + s / 3600
+              }
+            }
+            dd <- sign(d) * dd
+            if (tolower(h) == "n") {
+              coord$lat <- dd
+            } else if (tolower(h) == "s") {
+              coord$lat <- -dd
+            } else if (tolower(h) == "e") {
+              coord$lon <- dd
+            } else if (tolower(h) == "w") {
+              coord$lon <- -dd
+            }
           }
-        }
-        dd <- sign(d) * dd
-        if (tolower(h) == "n") {
-          coord$lat <- dd
-        } else if (tolower(h) == "s") {
-          coord$lat <- -dd
-        } else if (tolower(h) == "e") {
-          coord$lon <- dd
-        } else if (tolower(h) == "w") {
-          coord$lon <- -dd
         }
       }
 
+      result <- rbind(result, coord)
+
     }
-
-    result <- rbind(result, coord)
-
   }
 
   return(result)
--- tests/testthat/test_parsedms.R
+++ tests/testthat/test_parsedms.R
@@ -14,14 +14,15 @@ strings <- list(
   list(input = "51°28'38''N -101°16'56''E", output = c(-101.2822, 51.4772)),
   list(input = "51° N 101° W", output = c(-101.0000, 51.0000)),
   list(input = "51° N", output = c(NA, 51.0000)),
-  list(input = "12° N 109° 58’ 37” W", output = c(-109.9769, 12.0000))
+  list(input = "12° N 109° 58’ 37” W", output = c(-109.9769, 12.0000)),
+  list(input = "23.5", output = c(NA, NA))
 )
 
 for (i in 1:length(strings)) {
 
   test_that(paste0(strings[[i]]$input, " is parsed correctly"), {
     result <- parsedms(strings[[i]]$input)
-    expect_true(all.equal(strings[[i]]$output, as.numeric(result[1,]), tolerance = tolerance))
+    expect_true(all.equal(as.numeric(strings[[i]]$output), as.numeric(result[1,]), tolerance = tolerance))
   })
 
 }
